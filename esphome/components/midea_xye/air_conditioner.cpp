2e0b0c57c4a3242a3eee02c13e48d4df122c370c
Matthew Drobnak <matthew@drobnak.com>
2025-03-12 15:26:26 +0000

    Correct temperature calculation. 0x28, not 0x30 offset.

--- esphome/components/midea_xye/air_conditioner.cpp
+++ esphome/components/midea_xye/air_conditioner.cpp
@@ -357,7 +357,7 @@ uint32_t AirConditioner::CalculateGetTime(uint8_t time) {
 }
 
 float AirConditioner::CalculateTemp(uint8_t byte) {
-  return (byte - 0x30) / 2.0;
+  return (byte - 0x28) / 2.0;
 }
 
 ClimateTraits AirConditioner::traits() {
